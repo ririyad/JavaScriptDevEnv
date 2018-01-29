bda5d04d3f0689c0cc055f0f7878c4f0f428f188
ririyad <ririyad.291@gmail.com>
2018-01-29 11:06:26 +0600

    added local server

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,20 @@
+// this file will configure a web server that will serve up the files in the source directory
+
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3030;
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
